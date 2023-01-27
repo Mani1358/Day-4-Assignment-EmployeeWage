721e0cf5783da9fc37f013ce15e0a0b2fd9334a1
Mani1358 <mani621998@gmail.com>
2023-01-27 18:04:16 +0530

    Added:Day-4 EmpWage Welcome msg

---
+++ EmpWage.java
@@ -0,0 +1,7 @@
+package com.Java.day4assignment;
+
+public class EmpWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee" + "Wage Computation");
+    }
+}
